006e0a817596ea0190ad8ed5797ba55e53548ed8
lunar <2434717958@qq.com>
2026-08-19 17:34:34 +0800

    feat(build): rebuild publish bus impl, support unicast multicast broadcast.

--- luna_ps.c
+++ luna_ps.c
@@ -1,35 +1,135 @@
 /* luna_ps.c */
 #include "luna_ps.h"
 
-void luna_ps_init(struct luna_ps *ps, struct luna_bus *bus)
+void luna_ps_init(struct luna_ps *ps, struct luna_ps_config *config)
 {
 	LUNA_PS_ASSERT(ps);
-	LUNA_PS_ASSERT(bus);
-	ps->bus = *bus;
+	LUNA_PS_ASSERT(config);
+	ps->impl = config->impl;
+	ps->push = config->push;
+	ps->pop  = config->pop;
 }
 
-void luna_ps_attach(struct luna_ps *ps, const void *topic, const void *user)
+void luna_ps_attach(struct luna_ps *ps, void const *topic, void const *user)
 {
 	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(topic);
 	LUNA_PS_ASSERT(user);
-	if (ps->bus.attach) {
-		ps->bus.attach(ps, topic, user);
-	}
+	
+	LUNA_PS_ASSERT(ps->impl.attach);
+	ps->impl.attach(topic, user);
 }
 
-void luna_ps_detach(struct luna_ps *ps, const void *topic, const void *user)
+void luna_ps_detach(struct luna_ps *ps, void const *topic, void const *user)
 {
 	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(topic);
 	LUNA_PS_ASSERT(user);
-	if (ps->bus.detach) {
-		ps->bus.detach(ps, topic, user);
+
+	LUNA_PS_ASSERT(ps->impl.detach);
+	ps->impl.detach(topic, user);
+}
+
+static void luna_ps_push(struct luna_ps *ps, luna_ps_msg_t const *msg)
+{
+	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(ps->push);
+	ps->push(msg);
+}
+
+static bool luna_ps_pop(struct luna_ps *ps, luna_ps_msg_t const **msg)
+{
+	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(ps->pop);
+	return ps->pop(msg);
+}
+
+static void luna_ps_dispatch(struct luna_ps *ps, luna_ps_msg_t const *msg)
+{
+	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(msg);
+
+	switch (msg->msg_type) {
+	case LUNA_PS_MSG_UNICAST: {
+		ps->impl.unicast(msg->receiver, msg);
+	}
+        break;
+    	case LUNA_PS_MSG_MULTICAST: {
+		ps->impl.multicast(msg->topic, msg);
+	}
+        break;
+    	case LUNA_PS_MSG_BROADCAST: {
+		ps->impl.broadcast(msg);
+	}
+	break;
+	default:
+
+	break;
 	}
 }
 
-void luna_ps_publish(struct luna_ps *ps, const void *topic, void *data)
+luna_ps_msg_t *luna_ps_msg_alloc(size_t size)
+{
+	luna_ps_msg_t *msg = malloc(sizeof(luna_ps_msg_t) + size);
+	if (!msg) {
+		return NULL;
+	}
+	memset(msg, 0, sizeof(luna_ps_msg_t) + size);
+	msg->data = msg + 1;
+	msg->size = size;
+	return msg;
+}
+
+void luna_ps_msg_free(luna_ps_msg_t *msg)
+{
+	if (!msg) {
+		return;
+	}
+	free(msg);
+}
+
+void luna_ps_process(struct luna_ps *ps)
+{
+	luna_ps_msg_t const *msg = NULL;
+	if (luna_ps_pop(ps, &msg)) {
+		luna_ps_dispatch(ps, msg);
+		msg = NULL;
+	}
+}
+
+void luna_ps_unicast(struct luna_ps *ps, void const *sender, void const *target, luna_ps_msg_t *msg)
 {
 	LUNA_PS_ASSERT(ps);
-	if (ps->bus.publish) {
-		ps->bus.publish(ps, topic, data);
-	}
+	LUNA_PS_ASSERT(sender);
+	LUNA_PS_ASSERT(target);
+	LUNA_PS_ASSERT(msg);
+
+	msg->msg_type = LUNA_PS_MSG_UNICAST;
+	msg->sender   = sender;
+	msg->receiver = target;
+	luna_ps_push(ps, msg);
+}
+
+void luna_ps_multicast(struct luna_ps *ps, void const *sender, void const *topic, luna_ps_msg_t *msg)
+{
+	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(sender);
+	LUNA_PS_ASSERT(topic);
+	LUNA_PS_ASSERT(msg);
+
+	msg->msg_type = LUNA_PS_MSG_MULTICAST;
+	msg->sender   = sender;
+	msg->topic    = topic;
+	luna_ps_push(ps, msg);
+}
+
+void luna_ps_broadcast(struct luna_ps *ps, void const *sender, luna_ps_msg_t *msg)
+{
+	LUNA_PS_ASSERT(ps);
+	LUNA_PS_ASSERT(sender);
+	LUNA_PS_ASSERT(msg);
+
+	msg->msg_type = LUNA_PS_MSG_BROADCAST;
+	msg->sender   = sender;
+	luna_ps_push(ps, msg);
 }
--- luna_ps.h
+++ luna_ps.h
@@ -3,29 +3,65 @@
 #define LUNA_PS_H
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
 #ifndef LUNA_PS_ASSERT
-#include <assert.h>
-#define LUNA_PS_ASSERT		assert
+#define LUNA_PS_ASSERT(expr)	((void)(expr))
 #endif
 
-struct luna_ps;
+typedef enum {
+	LUNA_PS_MSG_UNSPECIFIED,
+	LUNA_PS_MSG_UNICAST,
+	LUNA_PS_MSG_MULTICAST,
+	LUNA_PS_MSG_BROADCAST,
+} luna_ps_msg_type_t;
 
-struct luna_bus {
-	void (*attach)(struct luna_ps *ps, const void *topic, const void *user);
-	void (*detach)(struct luna_ps *ps, const void *topic, const void *user);
-	void (*publish)(struct luna_ps *ps, const void *topic, void *data);
+typedef struct {
+	luna_ps_msg_type_t msg_type;
+
+	void              *topic;
+	void              *sender;
+	void              *receiver;
+
+	void              *data;
+	size_t             size;
+} luna_ps_msg_t;
+
+struct luna_ps_impl {
+	void (*attach)(void const *topic, void const *user);
+	void (*detach)(void const *topic, void const *user);
+
+	void (*unicast)(void const *user, luna_ps_msg_t const *msg);
+	void (*multicast)(void const *topic, luna_ps_msg_t const *msg);
+	void (*broadcast)(luna_ps_msg_t const *msg);
+};
+
+struct luna_ps_config {
+	struct luna_ps_impl impl;
+	void (*push)(luna_ps_msg_t const *msg);
+	bool (*pop)(luna_ps_msg_t const **msg);
 };
 
 struct luna_ps {
-	struct luna_bus bus;
+	struct luna_ps_impl impl;
+	void (*push)(luna_ps_msg_t const *msg);
+	bool (*pop)(luna_ps_msg_t const **msg);
 };
 
-void luna_ps_init(struct luna_ps *ps, struct luna_bus *bus);
-void luna_ps_attach(struct luna_ps *ps, const void *topic, const void *user);
-void luna_ps_detach(struct luna_ps *ps, const void *topic, const void *user);
-void luna_ps_publish(struct luna_ps *ps, const void *topic, void *data);
+void luna_ps_init(struct luna_ps *ps, struct luna_ps_config *config);
+void luna_ps_attach(struct luna_ps *ps, void const *topic, void const *user);
+void luna_ps_detach(struct luna_ps *ps, void const *topic, void const *user);
+
+void luna_ps_process(struct luna_ps *ps);
+
+
+luna_ps_msg_t *luna_ps_msg_alloc(size_t size);
+void luna_ps_msg_free(luna_ps_msg_t *msg);
+
+void luna_ps_unicast(struct luna_ps *ps, void const *sender, void const *target, luna_ps_msg_t *msg);
+void luna_ps_multicast(struct luna_ps *ps, void const *sender, void const *topic, luna_ps_msg_t *msg);
+void luna_ps_broadcast(struct luna_ps *ps, void const *sender, luna_ps_msg_t *msg);
 
 #endif
